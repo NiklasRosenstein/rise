dc2a29a31b0f8f05ca5f22648d9a7599a4805d1a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-11-29 11:45:09 +0100

    feat: dockerize rise-backend

--- rise-backend/src/settings.rs
+++ rise-backend/src/settings.rs
@@ -31,14 +31,14 @@ impl Settings {
 
         Config::builder()
             // Start off by merging in the "default" configuration file
-            .add_source(File::with_name("rise-backend/config/default.toml"))
+            .add_source(File::with_name("/config/default.toml"))
             // Add in the current environment file
             // Default to 'development' env
             // Note that this file is optional
-            .add_source(File::with_name(&format!("rise-backend/config/{}", run_mode)).required(false))
+            .add_source(File::with_name(&format!("/config/{}", run_mode)).required(false))
             // Add in a local configuration file
             // This file shouldn't be checked in to git
-            .add_source(File::with_name("rise-backend/config/local").required(false))
+            .add_source(File::with_name("/config/local").required(false))
             // Add in settings from the environment (with a prefix of APP)
             // Eg.. `APP_DEBUG=1` would set the `debug` key
             .add_source(Environment::with_prefix("RISE").separator("__"))
